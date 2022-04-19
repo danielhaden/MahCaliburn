f1e97824372d6951f42ff68e3ec600b87690bbec
Daniel Haden <haden.daniel@gmail.com>
2022-04-19 15:05:11 -0600

    Add project files.

---
+++ SimpleTabbedNavigation/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using System.Threading;
+
+namespace SimpleTabbedNavigation.ViewModels
+{
+    class ShellViewModel : Conductor<object>
+    {
+
+        public ShellViewModel()
+        {
+            ActivateItemAsync(IoC.Get<DemoViewModel>(), new CancellationToken());
+        }
+
+    }
+}
