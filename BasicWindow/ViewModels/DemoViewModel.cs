9f308a492379a0ca0ce1d6bc9acebb08720b45ff
Daniel Haden <haden.daniel@gmail.com>
2022-04-21 15:31:38 -0600

    added simple action binding example

--- BasicWindow/ViewModels/DemoViewModel.cs
+++ BasicWindow/ViewModels/DemoViewModel.cs
@@ -9,5 +9,35 @@ namespace BasicWindow.ViewModels
 {
     public class DemoViewModel : PropertyChangedBase
     {
+
+        /// <summary>
+        /// Stores the events aggregator
+        /// </summary>
+        private readonly IEventAggregator events;
+
+        public string ButtonDemoContent { get; set; } = "Hover mouse to see demo";
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="_events"></param>
+        public DemoViewModel(IEventAggregator _events)
+        {
+
+            events = _events;
+            events.SubscribeOnPublishedThread(this);
+
+        }
+
+        public async void OnSeeDialogWindowClick()
+        {
+            await Task.Run(() => Console.WriteLine("GOT HERE"));
+        }
+
+        public async void OnMouseHover()
+        {
+            await Task.Run(() => ButtonDemoContent = "!!!!!");
+            Refresh();
+        }
     }
 }
